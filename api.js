bdd1c70ec5d6cb3ea9727593b0fa2f94de9b9b11
knwagh <107362567+knwagh@users.noreply.github.com>
2022-08-22 15:46:50 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
-export const database = [
+
+  export const database = [
     { name: '21 Jump Street', imgUrl: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRLCqM8Ispa4waG8tNLPdy6rtiJFOEZUZxdzP-y_BQzfgo953Gb' },
     { name: '22 Jump Street', imgUrl: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQaoamRKQDYxVXvXg6LUl6brQmMdFbpZvOQ2G_nD6u5uq16tiVh' },
     { name: 'Cars', imgUrl: 'https://upload.wikimedia.org/wikipedia/en/3/34/Cars_2006.jpg' },
